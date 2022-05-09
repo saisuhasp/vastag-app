43af1acf00f140788282b90e6e48127734365e69
saisuhasp <saisuhaspsuhas@gmail.com>
2022-05-09 21:13:24 +0530

    added blockchain

---
+++ src/pages/SignupPages/CusABI.js
@@ -0,0 +1,38 @@
+export const CusABI =[
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_message",
+				"type": "string"
+			}
+		],
+		"name": "addData",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getMessage",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
--- src/pages/SignupPages/SignupCus.js
+++ src/pages/SignupPages/SignupCus.js
@@ -1,6 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
+import "./style1.css"
+import Web3 from "web3";
+import { CusABI } from "./CusABI";
 
-export default function SignupCus() {
+const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+web3.eth.defaultAccount = web3.eth.accounts[0];
+
+const RemixContract = new web3.eth.Contract(
+  CusABI,
+  "0xE4af6670aB371Dc54d4D0c8f1ECc277a7c497B0A"
+);
+ function SignupCus() {
+    const [customer, setCustomer] = useState([]);
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [phoneNo,setPhoneNo]=useState('')
+    const [gender,setGender]=useState('')
+    const [address,setAddres]=useState('')
+    const [city,setCity]=useState('')
+    const [state,setState]=useState('')
+
+  
+  const [data,setstr]=useState()
+  
+  
+      const setData= async e=>{
+      
+        e.preventDefault();
+  
+        const accounts = await window.ethereum.enable();
+      const account = accounts[0];
+     
+      var lst=[
+        ...customer,{name,email,phoneNo,gender,address,city,state}
+      ];
+      let str=""
+      for(let i in lst[0]){
+        let v=lst[0]
+        str=str+String(i)+"_"+String(v[i])+"_"
+      }   
+      const gas = await RemixContract.methods.addData(str).estimateGas();
+      
+      const result = await RemixContract.methods
+        .addData(str)
+        .send({ from: account, gas });
+      console.log(result);
+    }
+  
+  
+    const [dataset,setDatatobody]=useState()
+    const getData = async e => {
+      RemixContract.methods
+        .getMessage()
+        .call()
+        .then((res)=>{setDatatobody(res)});
+      console.log(dataset)
+    };
     return (
         <div className="page">
             <div className="container">
@@ -10,16 +65,16 @@ export default function SignupCus() {
                         <div className="user-details">
                             <div className="input-box">
                                 <span className="details">Full Name</span>
-                                <input type="text" placeholder="Enter your name" required />
+                                <input type="text" placeholder="Enter your name" required value={name} onChange={(e)=>setName(e.target.value)} />
                             </div>
 
                             <div className="input-box">
                                 <span className="details">Email</span>
-                                <input type="email" placeholder="Enter your email" required />
+                                <input type="email" placeholder="Enter your email" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Phone Number</span>
-                                <input type="text" placeholder="Enter your number" required />
+                                <input type="text" placeholder="Enter your number" required value={phoneNo} onChange={(e)=>setPhoneNo(e.target.value)} />
                             </div>
                             <div className="input-box">
                                 <span className="details">Password</span>
@@ -31,50 +86,32 @@ export default function SignupCus() {
                             </div>
                             <div className="input-box">
                                 <span className="details">Address</span>
-                                <input type="text" placeholder="Enter your Address" required />
+                                <input type="text" placeholder="Enter your Address" required value={address} onChange={(e)=>setAddres(e.target.value)} />
                             </div>
                             <div className="input-box">
                                 <span className="details">City</span>
-                                <input type="text" placeholder="Enter your Address" required />
+                                <input type="text" placeholder="Enter your Address" required value={city} onChange={(e)=>setCity(e.target.value)} />
                             </div>
                             <div className="input-box">
                                 <span className="details">State</span>
-                                <input type="text" placeholder="Enter your Address" required />
+                                <input type="text" placeholder="Enter your Address" required value={state} onChange={(e)=>setState(e.target.value)}/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Gender</span>
+                                <input type="text" placeholder="Enter your gender" required value={gender} onChange={e=>setGender(e.target.value)}/>
                             </div>
 
                         </div>
-                        <div className="gender-details">
-                            <input type="radio" name="gender" id="dot-1" />
-                            <input type="radio" name="gender" id="dot-2" />
-                            <input type="radio" name="gender" id="dot-3" />
-                            <span className="gender-title">Gender</span>
-                            <div className="category">
-                                <label for="dot-1">
-                                    <span className="dot one"></span>
-                                    <span className="gender">Male</span>
-                                </label>
-                                <label for="dot-2">
-                                    <span className="dot two"></span>
-                                    <span className="gender">Female</span>
-                                </label>
-                                <label for="dot-3">
-                                    <span className="dot three"></span>
-                                    <span className="gender">Prefer not to say</span>
-                                </label>
-                            </div>
-                        </div>
-                        <div className="documents">
-
-                            <p className="details">Add Profile Picture</p>
-                            <input type="file" id="myFile" name="filename" required />
-                        </div>
-
+    
                         <div className="button">
-                            <input type="submit" value="Register" />
+                            <input type="submit" value="Register" onClick={setData} />
                         </div>
+                        
                     </form>
                 </div>
             </div>
         </div>
     )
 }
+
+export default SignupCus;
